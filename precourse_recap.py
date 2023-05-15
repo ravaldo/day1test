cc8b6781f36c9ce447c6d2156c6863d33c15abc2
ravaldo <117010252+ravaldo@users.noreply.github.com>
2023-05-15 15:41:24 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,16 @@
+# create variables of different types
+# call some methods/functions on these variables
+# use some operators (e.g. +, -)
+
+
+
+my_int = 10
+my_float = 2.5
+my_bool = True
+my_string = "Hello World!"
+
+product = my_int / my_float
+
+print(product)
+
+print(my_string.upper() + " How you doin'?")
